3e1a8e089ad255e39ba3a090d102d60eafd5f7e9
Effectuss <panovsavelii@gmail.com>
2023-07-31 18:58:29 +0700

    Day00 ex01

--- Day00/src/ex01/Program.java
+++ Day00/src/ex01/Program.java
@@ -1,7 +1,36 @@
 package ex01;
 
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("asd");
+        Scanner scanner = new Scanner(System.in);
+        isPrimeNumber(scanner.nextInt());
+        scanner.close();
     }
+
+    public static void isPrimeNumber(int number) {
+        if(!isCorrectInput(number)) {
+            System.err.println("Illegal Argument");
+            System.exit(-1);
+        }
+        int iterationCount = 1;
+        boolean isPrime = true;
+        for(int i = 2; i <= Math.sqrt(number); ++i, ++iterationCount) {
+            if ((number % i) == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            System.out.println("true " + iterationCount);
+        } else {
+            System.out.println("false " + iterationCount);
+        }
+    }
+
+    public static boolean isCorrectInput(int number) {
+        return (number > 0) && (number != 1);
+    }
+
 }
