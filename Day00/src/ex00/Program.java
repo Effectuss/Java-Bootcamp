fbd40ee337cd60113fe4e99ba83c5e16164dcc8e
English Kozanset <englishk@ob-k2.nsk.21-school.ru>
2023-07-31 15:42:10 +0700

    31.07 - Day00 ex00

---
+++ Day00/src/ex00/Program.java
@@ -0,0 +1,19 @@
+package ex00;
+
+public class Program {
+    public static void main(String[] args) {
+        Program.sumSixDigitsNumber();
+    }
+
+    public static void sumSixDigitsNumber() {
+        int number = 479598;
+        int sum = 0;
+
+        while(number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        System.out.println(sum);
+    }
+}
