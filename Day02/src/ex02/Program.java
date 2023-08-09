6ed3da7f6710ca155efeec6e60038416b6c68db6
Effectuss <panovsavelii@gmail.com>
2023-08-09 14:11:09 +0700

    Day02 ex02 validation input data

--- Day02/src/ex02/Program.java
+++ Day02/src/ex02/Program.java
@@ -1,7 +1,25 @@
 package ex02;
 
+import java.nio.file.Path;
+
 public class Program {
     public static void main(String[] args) {
+        if (!isValidInputArgument(args)) {
+            System.exit(-1);
+        }
 
+    }
+
+    static private boolean isValidInputArgument(String[] arguments) {
+        if (arguments.length != 1) {
+            System.out.println("Incorrect number of arguments!");
+            return false;
+        }
+        Path startPath = Path.of(arguments[0].split("=")[Math.max(0, arguments[0].split("=").length - 1)]);
+        if (!startPath.isAbsolute()) {
+            System.out.println("The path should be absolute!");
+            return false;
+        }
+        return true;
     }
 }
