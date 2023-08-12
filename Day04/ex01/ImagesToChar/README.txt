ad370e54e90f746c2d039aca2d4cdf76850e03b1
Effectuss <panovsavelii@gmail.com>
2023-08-12 17:56:10 +0700

    Day04 ex02 finish

---
+++ Day04/ex01/ImagesToChar/README.txt
@@ -0,0 +1,17 @@
+#1. Delete target directory if exist
+rm -rf target
+
+#2. Create a target directory
+mkdir target
+
+#3. Compile files to the target directory
+javac -d target src/java/edu/school21/printer/app/Program.java src/java/edu/school21/printer/logic/ConsoleImagePrinter.java
+
+#4 Copy resources to target
+cp -r src/resources target/.
+
+#5 Create jar:
+jar cvfm ./target/images-to-chars-printer.jar src/manifest.txt -C target .
+
+#6 Run program
+java -jar target/images-to-chars-printer.jar . 0
--- Day04/ex01/ImagesToChar/src/java/edu/school21/printer/app/Program.java
+++ Day04/ex01/ImagesToChar/src/java/edu/school21/printer/app/Program.java
@@ -1,7 +1,30 @@
 package edu.school21.printer.app;
 
+import edu.school21.printer.logic.ConsoleImagePrinter;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Program {
     public static void main(String[] args) {
+        try {
+            checkProgramArgument(args);
+            ConsoleImagePrinter imagePrinter = new ConsoleImagePrinter(new File("/resources/it.bmp"),
+                    args[0].charAt(0), args[1].charAt(0));
+            imagePrinter.print();
+        } catch (IllegalArgumentException | IOException e) {
+            System.err.println(e.getMessage());
+        }
 
     }
+
+    private static void checkProgramArgument(String[] inputArgs) throws IllegalArgumentException {
+        if (inputArgs.length != 2) {
+            throw new IllegalArgumentException("Input arguments count must be 2");
+        }
+        if (inputArgs[0].length() != 1 || inputArgs[1].length() != 1) {
+            throw new IllegalArgumentException("The first and second arguments must be single characters");
+        }
+    }
+
 }
