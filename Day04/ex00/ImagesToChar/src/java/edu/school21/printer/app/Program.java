f88b44570862233609560c97f6d30f8a5925c13c
Effectuss <panovsavelii@gmail.com>
2023-08-12 14:12:10 +0700

    Day04 ex00 tree directory

---
+++ Day04/ex00/ImagesToChar/src/java/edu/school21/printer/app/Program.java
@@ -0,0 +1,10 @@
+package edu.school21.printer.app;
+
+import edu.school21.printer.logic.ImagesConvertor;
+
+public class Program {
+    public static void main(String[] args) {
+        ImagesConvertor convertor = new ImagesConvertor();
+    }
+
+}
