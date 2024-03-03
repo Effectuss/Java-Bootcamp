15c0d93291adca9e968faf4604179fa27198a069
English Kozanset <englishk@si-b2.nsk.21-school.ru>
2024-03-03 17:42:11 +0700

    schema for bd

--- Day05/chat/src/main/java/edu/school21/chat/app/Main.java
+++ Day05/chat/src/main/java/edu/school21/chat/app/Main.java
@@ -1,7 +1,13 @@
 package edu.school21.chat.app;
 
+import edu.school21.chat.models.Chatroom;
+
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-
+        Date date = new Date(System.currentTimeMillis());
+        Chatroom chatroom = new Chatroom();
+        System.out.print(chatroom.getId());
     }
 }
--- Day05/chat/src/main/java/edu/school21/chat/models/Chatroom.java
+++ Day05/chat/src/main/java/edu/school21/chat/models/Chatroom.java
@@ -1,4 +1,10 @@
 package edu.school21.chat.models;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
 public class Chatroom {
+    private Long id = 100L;
 }
