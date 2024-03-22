f996289c7c2d281a30ad8d3fb61ebb1cca672903
Effectuss <panovsavelii@gmail.com>
2024-03-22 18:50:47 +0700

    Day06. ex02 add some info to test.

--- JUnit-Mockito/Tests/src/test/java/edu/school21/repositories/ProductsRepositoryJdbcImplTest.java
+++ JUnit-Mockito/Tests/src/test/java/edu/school21/repositories/ProductsRepositoryJdbcImplTest.java
@@ -2,14 +2,44 @@ package edu.school21.repositories;
 
 
 import edu.school21.models.Product;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import repositories.ProductRepository;
+import repositories.ProductRepositoryJdbcImpl;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProductsRepositoryJdbcImplTest {
+    private EmbeddedDatabase db;
+    private ProductRepository productRepository;
+
     // TODO создать норм объекты
-    final List<Product> EXPECTED_FIND_ALL_PRODUCTS = new ArrayList<>();
-    final Product EXPECTED_FIND_BY_ID_PRODUCT = new Product(1L,"asd",new BigDecimal("11.22"));
-    final Product EXPECTED_UPDATED_PRODUCT = new Product(1L,"asd",new BigDecimal("11.22"));
+    final List<Product> EXPECTED_FIND_ALL_PRODUCTS = List.of();
+    final Product EXPECTED_FIND_BY_ID_PRODUCT;
+    final Product EXPECTED_UPDATED_PRODUCT;
+    final Product EXPECTED_SAVE_PRODUCT;
+    final List<Product> EXPECTED_PRODUCTS_AFTER_DELETE = List.of();
+    final
+
+
+    @BeforeEach
+    void init() {
+        db = new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(EmbeddedDatabaseType.HSQL)
+                .addScript("schema.sql")
+                .addScript("data.sql")
+                .build();
+        productRepository = new ProductRepositoryJdbcImpl(db);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        db.shutdown();
+    }
 }
