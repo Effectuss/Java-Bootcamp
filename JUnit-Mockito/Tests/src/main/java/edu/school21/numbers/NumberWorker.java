80a841b3b37574448bddef7fb8f27c87154c12a7
Effectuss <panovsavelii@gmail.com>
2024-03-22 15:35:05 +0700

    Day06. ex00 done.

--- JUnit-Mockito/Tests/src/main/java/edu/school21/numbers/NumberWorker.java
+++ JUnit-Mockito/Tests/src/main/java/edu/school21/numbers/NumberWorker.java
@@ -1,4 +1,38 @@
 package edu.school21.numbers;
 
+import edu.school21.numbers.exception.IllegalNumberException;
+
 public class NumberWorker {
+    private static final String INVALID_NUMBER_FOR_PRIME =
+            "This number %d cannot be verified for prime!\n" +
+                    "Number must be greater than 1 !";
+
+    public boolean isPrime(int number) {
+        if (number <= 1) {
+            throw new IllegalNumberException(String.format(INVALID_NUMBER_FOR_PRIME, number));
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); ++i) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public int digitSum(int number) {
+        if (number < 0) {
+            number *= -1;
+        }
+
+        int digitSum = 0;
+
+        while (number > 0) {
+            digitSum += number % 10;
+            number /= 10;
+        }
+
+        return digitSum;
+    }
 }
--- JUnit-Mockito/Tests/src/test/java/edu/school21/NumberWorkerTest.java
+++ JUnit-Mockito/Tests/src/test/java/edu/school21/NumberWorkerTest.java
@@ -1,5 +1,38 @@
 package edu.school21;
 
-public class NumberWorkerTest {
+import edu.school21.numbers.NumberWorker;
+import edu.school21.numbers.exception.IllegalNumberException;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberWorkerTest {
+    private static final NumberWorker numberWorker = new NumberWorker();
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 7, 11, 13})
+    void isPrimeForPrimes(int primeNumber) {
+        assertTrue(numberWorker.isPrime(primeNumber));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 6, 10})
+    void isPrimeForNotPrimes(int notPrimeNumber) {
+        assertFalse(numberWorker.isPrime(notPrimeNumber));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, -10})
+    void isPrimeForIncorrectNumbers(int incorrectNumber) {
+        assertThrows(IllegalNumberException.class, () ->
+                numberWorker.isPrime(incorrectNumber));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv")
+    void testDigitSum(int number, int sum) {
+        assertEquals(sum, numberWorker.digitSum(number));
+    }
 }
