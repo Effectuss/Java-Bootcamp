3cc585f6aa85f3f4ffd3a9be6c2ddc1ddc8d73c4
Effectuss <panovsavelii@gmail.com>
2024-03-30 17:36:06 +0700

    Day06. ex03 not finish mockito.

--- JUnit-Mockito/Tests/src/test/java/edu/school21/services/UsersServiceImplTest.java
+++ JUnit-Mockito/Tests/src/test/java/edu/school21/services/UsersServiceImplTest.java
@@ -7,50 +7,57 @@ import edu.school21.repositories.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import java.sql.SQLException;
 
+class UsersServiceImplTest {
 
-public class UsersServiceImplTest {
-    private static final String CORRECT_LOGIN = "CORRECT_LOGIN";
-    private static final String INCORRECT_LOGIN = "INCORRECT_LOGIN";
-    private static final String CORRECT_PASSWORD = "CORRECT_PASSWORD";
-    private static final String INCORRECT_PASSWORD = "INCORRECT_PASSWORD";
+    private static final String CORRECT_LOGIN = "correct_login";
+    private static final String CORRECT_PASSWORD = "correct_password";
+    private static final String INCORRECT_LOGIN = "incorrect_login";
+    private static final String INCORRECT_PASSWORD = "incorrect_password";
 
+    @Mock
     private UserRepository userRepository;
     private UsersServiceImpl usersService;
-    private User user;
 
     @BeforeEach
     void setUp() {
-        user = new User(1L, CORRECT_LOGIN, CORRECT_PASSWORD, false);
-        userRepository = Mockito.mock(UserRepository.class);
+        MockitoAnnotations.openMocks(this);
         usersService = new UsersServiceImpl(userRepository);
     }
 
     @Test
     void testCorrectLoginAndPassword() throws SQLException, EntityNotFoundException, AlreadyAuthenticatedException {
+        User user = new User(1L, CORRECT_LOGIN, CORRECT_PASSWORD, false);
         Mockito.when(userRepository.findByLogin(CORRECT_LOGIN)).thenReturn(user);
 
         Assertions.assertTrue(usersService.authenticate(CORRECT_LOGIN, CORRECT_PASSWORD));
         Assertions.assertTrue(user.isAuthenticated());
+
         Mockito.verify(userRepository).update(user);
     }
 
     @Test
     void testIncorrectLogin() throws SQLException, EntityNotFoundException {
-        Assertions.assertThrows(EntityNotFoundException.class,
-                () -> userRepository.findByLogin(INCORRECT_LOGIN));
+        Mockito.when(userRepository.findByLogin(INCORRECT_LOGIN)).thenThrow(EntityNotFoundException.class);
 
         Assertions.assertThrows(EntityNotFoundException.class,
                 () -> usersService.authenticate(INCORRECT_LOGIN, CORRECT_PASSWORD));
 
-        Mockito.verify(userRepository, Mockito.never()).update(user);
+        Mockito.verify(userRepository, Mockito.never()).update(Mockito.any());
     }
 
     @Test
-    void testIncorrectPassword() throws AlreadyAuthenticatedException {
+    void testIncorrectPassword() throws AlreadyAuthenticatedException, SQLException, EntityNotFoundException {
+        User user = new User(1L, CORRECT_LOGIN, CORRECT_PASSWORD, false);
+        Mockito.when(userRepository.findByLogin(CORRECT_LOGIN)).thenReturn(user);
 
+        Assertions.assertFalse(usersService.authenticate(CORRECT_LOGIN, INCORRECT_PASSWORD));
+
+        Mockito.verify(userRepository, Mockito.never()).update(Mockito.any());
     }
 }
