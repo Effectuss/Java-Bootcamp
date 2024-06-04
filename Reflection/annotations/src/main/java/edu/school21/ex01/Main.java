27dde6b6cd734de8ffd4012a2155886132041629
Panov Savelii <s.panov@cft.ru>
2024-06-04 16:59:29 +0700

    Day07. Base struct for all day

---
+++ Reflection/annotations/src/main/java/edu/school21/ex01/Main.java
@@ -0,0 +1,9 @@
+package edu.school21.ex01;
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- Reflection/orm/src/main/java/edu/school21/ex00/main/App.java
+++
@@ -1,7 +0,0 @@
-package edu.school21.ex00.main;
-
-public class App {
-    public static void main(String[] args) {
-
-    }
-}
