6438a6acace6604d454b721281d30a58b7cebca5
Effectuss <panovsavelii@gmail.com>
2024-06-16 17:45:58 +0700

    Day07. ex02 base struct ready, need implement methods.

--- Reflection/orm/src/main/java/edu/school21/Main.java
+++ Reflection/orm/src/main/java/edu/school21/Main.java
@@ -1,9 +1,47 @@
 package edu.school21;
 
-public class Main
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import edu.school21.annotations.OrmColumn;
+import edu.school21.db.DbConfig;
+import edu.school21.entities.User;
+import edu.school21.manager.OrmManager;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        try {
+            DbConfig dbConfig = new DbConfig();
+
+            HikariConfig hikariConfig = configureHikari(dbConfig);
+
+            try (HikariDataSource hikariDataSource = new HikariDataSource(hikariConfig);
+                 OrmManager ormManager = new OrmManager(hikariDataSource)) {
+
+                User user = new User(1L, "asd", "asd", 3);
+
+                System.out.println(user.getFirstName());
+                String fieldName = "firstName";
+
+                OrmColumn ormColumn = User.class.getDeclaredField(fieldName).getAnnotation(OrmColumn.class);
+
+                if (ormColumn != null) {
+                    System.out.println("Column name: " + ormColumn.name());
+                } else {
+                    System.out.println("No OrmColumn annotation found on field: " + fieldName);
+                }
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private static HikariConfig configureHikari(DbConfig dbConfig) {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(dbConfig.getUrl());
+        hikariConfig.setUsername(dbConfig.getUsername());
+        hikariConfig.setPassword(dbConfig.getPassword());
+        return hikariConfig;
     }
 }
---
+++ Reflection/orm/src/main/java/edu/school21/manager/OrmManager.java
@@ -0,0 +1,55 @@
+package edu.school21.manager;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public final class OrmManager implements AutoCloseable {
+    private final Connection connection;
+
+    public OrmManager(DataSource dataSource) throws SQLException {
+        this.connection = dataSource.getConnection();
+        dropEntityTables();
+        initialize();
+    }
+
+    private void dropEntityTables() {
+    }
+
+    private void initialize() {
+        createTable();
+    }
+
+    private void createTable() {
+    }
+
+    public void save(Object entity) {
+    }
+
+    public void update(Object entity) {
+    }
+
+    public <T> T findById(Long id, Class<T> clazz) {
+        return null;
+    }
+
+    public void begin() throws SQLException {
+        connection.setAutoCommit(false);
+    }
+
+    public void commit() throws SQLException {
+        connection.commit();
+    }
+
+    public void rollback() throws SQLException {
+        connection.rollback();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+
+}
